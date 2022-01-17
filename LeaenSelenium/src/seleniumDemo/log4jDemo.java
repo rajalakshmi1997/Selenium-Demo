447d60976e70e93ac8ee8f7e0eaf72a1d114b640
rajalakshmi1997 <97144329+rajalakshmi1997@users.noreply.github.com>
2022-01-17 12:14:51 +0530

    Update log4jDemo.java

--- LeaenSelenium/src/seleniumDemo/log4jDemo.java
+++ LeaenSelenium/src/seleniumDemo/log4jDemo.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+//code change
+
 public class log4jDemo {
 
 	public static void main(String[] args) {
